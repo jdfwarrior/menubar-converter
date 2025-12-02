6faed2a2435c76036dd91c27b0b58984aaeef5c1
David Ferguson <david@ferg.co>
2025-12-01 20:07:46 -0600

    bump version

--- Casks/mbconverter.rb
+++ Casks/mbconverter.rb
@@ -1,5 +1,5 @@
 cask "mbconverter" do
-  version "0.0.2"
+  version "0.0.3"
   sha256 "4ff3b5d0191e9a982d29e25c74b94a56ad7fd2d58d81eed2cf3b60991a614565"
 
   url "https://github.com/jdfwarrior/menubar-converter/releases/download/v#{version}/mbconverter-#{version}.zip"
